4917e3fbf36e476268a24c0e2e634ec5025009c4
omerKaduri1 <kaduriomer@gmail.com>
2024-05-05 18:53:23 +0300

    Add loader service

---
+++ src/app/services/loader.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoaderService {
+
+  private _isLoading$ = new BehaviorSubject<boolean>(false)
+  public isLoading$ = this._isLoading$.asObservable()
+
+  setIsLoading(isLoading: boolean) {
+    this._isLoading$.next(isLoading)
+  }
+
+  constructor() { }
+}
